371f327e844ea4fdcc6f8a9226ced53bbcf4602d
Suhyun An <movptr06@outlook.kr>
2024-03-02 12:52:12 +0900

    Add: Scoreboard

---
+++ ux/static/js/scoreboard.js
@@ -0,0 +1,25 @@
+function appendScore(rank, username, score) {
+    var $tr = document.createElement("tr");
+
+    var $td_rank = document.createElement("td");
+    $td_rank.innerText = rank.toString();
+    $tr.appendChild($td_rank);
+
+    var $td_username = document.createElement("td");
+    $td_username.innerText = username;
+    $tr.appendChild($td_username);
+
+    var $td_score = document.createElement("td");
+    $td_score.innerText = score.toString();
+    $tr.appendChild($td_score)
+
+    document.getElementById("scoreboard-table").appendChild($tr);
+}
+
+function main() {
+    for(var i = 1; i < 500; i += 1) {
+        appendScore(i, "movptr", 31337);
+    }
+}
+
+window.onload = main;
